69484fb29c6ba8e77a99912e39bafeda28a2863c
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2024-07-22 01:59:58 -0600

    Page unprotected for create tickets

--- src/app/tickets/create/page.tsx
+++ src/app/tickets/create/page.tsx
@@ -1,12 +1,9 @@
-import ProtectedRoute from "@/components/ProtectedRoute";
 import CreateTicket from "@/components/CreateTicket";
 
 export default function CreateTicketPage() {
   return (
-    <ProtectedRoute>
-      <div>
-        <CreateTicket />
-      </div>
-    </ProtectedRoute>
+    <div>
+      <CreateTicket />
+    </div>
   );
 }
